8a2b6dbcd9262f878a9db82a090772fdcf77a206
gaoziqiang <gaoziqianghi@.com>
2016-12-16 10:14:11 +0800

    房间预定代码

---
+++ src/main/java/cn/edu/sdut/softlab/rooms/Room.java
@@ -0,0 +1,46 @@
+package cn.edu.sdut.softlab.rooms;
+
+public class Room {
+	private String no; // 房间号
+	private RoomStatus status; // 状态
+	private RoomType type; // 人数
+
+    public Room(){}
+
+    public Room(String no, RoomStatus status, RoomType type) {
+        this.no = no;
+        this.status = status;
+        this.type = type;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public RoomStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(RoomStatus status) {
+        this.status = status;
+    }
+
+    public RoomType getType() {
+        return type;
+    }
+
+    public void setType(RoomType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" + "no=" + no + ", status=" + status + ", type=" + type + '}';
+    }
+    
+
+}
---
+++ src/main/java/cn/edu/sdut/softlab/rooms/RoomStatus.java
@@ -0,0 +1,16 @@
+package cn.edu.sdut.softlab.rooms;
+
+public enum RoomStatus {
+    CHECKED_IN("已入住"), // 客户已入住状态
+    AVAILABLE("空闲"); // 空闲状态
+
+    private String label;
+
+    private RoomStatus(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
